b2b224ca677307bee2e839904a9c4656f0fb77e2
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-05-05 18:53:39 -0400

    add results content, bg colors, font weights

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,7 +1,17 @@
+import "./Result.css";
+
 export default function Result() {
   return (
-    <div>
-      <p>i have results</p>
+    <div className="result-section">
+      <p className="font-lavender result-header">Your Result</p>
+      <div className="circle">
+        <p className="results-text result-score">75</p>
+        <p className="results-text result-of-100">of 100</p>
+      </div>
+      <h3 className="results-text result-descriptor">Great</h3>
+      <p className="font-lavender result-comparison">
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
     </div>
   );
 }
