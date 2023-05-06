f87598c91dc52b7217802d1113e4c7a4796e9a6e
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-05-06 12:01:49 -0400

    first draft of one ListItem, very rough

---
+++ src/components/Elements/ListItem.tsx
@@ -0,0 +1,22 @@
+interface ListItemProps {
+  svgUrl: string;
+  category: string;
+  score: number;
+  className: string;
+}
+
+export default function ListItem(props: ListItemProps) {
+  const { svgUrl, category, score, className } = props;
+  return (
+    <div className={className}>
+      <div className="flex space-between">
+        <img src={svgUrl}></img>
+        <p>{category}</p>
+      </div>
+      <div className="flex space-between">
+        <p>{score}</p>
+        <p> / 100</p>
+      </div>
+    </div>
+  );
+}
--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,11 +1,42 @@
 import "./Summary.css";
 import Button from "../Elements/Button";
+import ListItem from "../Elements/ListItem";
+import reaction from "../../assets/images/icon-reaction.svg";
 
 export default function Summary() {
   return (
     <div className="summary-section">
       <p>a summary of Sumeria</p>
+      <ListItem
+        className="list-item"
+        svgUrl={reaction}
+        category={data[0].category}
+        score={data[0].score}
+      />
       <Button className="summary-button" displayText="Continue" />
     </div>
   );
 }
+
+const data = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: "../../assets/images/icon-reaction.svg",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: "../assets/images/icon-memory.svg",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: "../assets/images/icon-verbal.svg",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: "../assets/images/icon-visual.svg",
+  },
+];
