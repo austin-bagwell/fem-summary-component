09781d740b5aedf9722d5b97c592da07ff5f5c07
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-05-07 15:00:10 -0400

    adjust category styling, fix result typo

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -5,11 +5,11 @@ export default function Result() {
     <div className="result-section">
       <p className="font-lavender result-header">Your Result</p>
       <div className="circle">
-        <p className="results-text result-score">75</p>
+        <p className="results-text result-score">76</p>
         <p className="results-text result-of-100">of 100</p>
       </div>
       <h3 className="results-text result-descriptor">Great</h3>
-      <p className="font-lavender result-comparison">
+      <p className="font-lavender font-xbold result-comparison">
         You scored higher than 65% of the people who have taken these tests.
       </p>
     </div>
